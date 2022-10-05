242becbe6c8c7d4b75deb53d2472f6a22c348c78
ConnorHoare <connor@tipcigroup.com>
2022-10-05 19:30:05 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,21 +1,41 @@
 // Global variables
 const otherJobRole = document.getElementById("other-job-role");
 const jobRoleSelects = document.getElementById("title");
+
 const colorSelects = document.getElementById("color");
 const designSelects = document.getElementById("design");
+
+const activities = document.getElementById("activities-box");
+const activitiesCost = document.getElementById("activities-cost");
+let counter = 0;
+
+const paymentSelects = document.getElementById("payment");
+const creditCard = document.getElementById("credit-card");
+const paypal = document.getElementById("paypal");
+const bitcoin = document.getElementById("bitcoin");
+
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+
+let checkedArr = [];
+
 
 // When website page loads
 window.onload = () => {
 
     // Get the first input element and set the focus method to prompt user to start input here
-    const getFirstInput = document.getElementById("name");
-    getFirstInput.focus();
+
+    nameInput.focus();
 
     // Hide the other job role field
     otherJobRole.style.display = "none";
 
     // Disable the color dropdown
     colorSelects.disabled = true;
+
+    // Set the creditCard as the default payment
+    paypal.style.display = "none";
+    bitcoin.style.display = "none";
 }
 
 // function to change show text field when user selects other job role
@@ -75,6 +95,24 @@ function displayHeartJsColors() {
     }
   }
 }
+// Show creditCard
+function showCreditCardPayment() {
+  creditCard.style.display = "block";
+  paypal.style.display = "none";
+  bitcoin.style.display = "none";
+}
+// Show PayPal
+function showPayPalPayment() {
+  creditCard.style.display = "none";
+  paypal.style.display = "block";
+  bitcoin.style.display = "none";
+}
+// Show bitcoin
+function showBitcoinPayment() {
+  creditCard.style.display = "none";
+  paypal.style.display = "none";
+  bitcoin.style.display = "block";
+}
 
 // display text field based on selection
 jobRoleSelects.onchange = () => { displayOtherJobRole() }
@@ -88,3 +126,59 @@ designSelects.onchange = () => {
     displayHeartJsColors()
   }
 }
+
+// Detect changes on the activties inputs
+activities.onchange = (e) => {
+  const clicked = e.target;
+  const dataCost = clicked.getAttribute("data-cost");
+  const parsedCost = parseInt(dataCost);
+  if (clicked.checked) {
+    counter += parsedCost
+    activitiesCost.innerText = `Total: $${counter}`;
+    checkedArr.push(clicked)
+  } else if (clicked.checked === false) {
+    counter -= parsedCost
+    activitiesCost.innerText = `Total: $${counter}`;
+    checkedArr.pop(clicked);
+  }
+}
+
+// Detect change on the paymentSelects
+paymentSelects.onchange = () => {
+  if (paymentSelects.value === "credit-card") {
+    showCreditCardPayment()
+  }
+  if (paymentSelects.value === "paypal") {
+    showPayPalPayment()
+  }
+  if (paymentSelects.value === "bitcoin") {
+    showBitcoinPayment()
+  }
+}
+
+const button = document.querySelector('button');
+button.addEventListener("click", function(e) {
+
+// Check is name field is empty
+  if (nameInput.value == 0) {
+    e.preventDefault();
+    alert("Add your name");
+  }
+
+// Check for correct email formatting
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (emailInput.value.match(mailformat)) {
+    return true
+  } else {
+    e.preventDefault();
+    alert("Invalid address");
+  }
+
+// Check if activities are unselected
+  if (checkedArr.length === 0) {
+    e.preventDefault();
+    alert("Activity must be selected");
+  }
+
+
+});
